2a2f738d818c3c375e96f6b63753035e6b611172
shixingang <shi_xin_gang@163.com>
2025-05-24 09:31:08 +0800

    优化

--- app/vfd/src/user/main.c
+++ app/vfd/src/user/main.c
@@ -61,7 +61,7 @@ VOID  tx_application_define(VOID *first_unused_memory)
 
 static  void  taskstart (ULONG thread_input)
 {
-	(void)thread_input;
+  (void)thread_input;
 
     log_init();
 
@@ -69,7 +69,7 @@ static  void  taskstart (ULONG thread_input)
 
 	nx_msg_init();
 
-    service_test_start();
+    //service_test_start();
     
     service_data_start();
 
@@ -89,7 +89,6 @@ static  void  taskstart (ULONG thread_input)
   * @brief System Clock Configuration
   * @retval None
   */
-#if 1 /*170M*/
 static void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -134,52 +133,7 @@ static void SystemClock_Config(void)
   */
   HAL_RCC_EnableCSS();
 }
-#else /*160M*/
- void SystemClock_Config(void)
- {
-   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
- 
-   /** Configure the main internal regulator output voltage
-   */
-   HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
- 
-   /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
-   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-   RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV3;
-   RCC_OscInitStruct.PLL.PLLN = 40;
-   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-   {
-     Error_Handler();
-   }
- 
-   /** Initializes the CPU, AHB and APB buses clocks
-   */
-   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
- 
-   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-   {
-     Error_Handler();
-   }
- 
-   /** Enables the Clock Security System
-   */
-   HAL_RCC_EnableCSS();
- }
-#endif
+
 
 
 /**
